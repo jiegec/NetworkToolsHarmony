d41ee71016f4bae4bb09bcfefdbbb6af9605ffe2
Jiajie Chen <c@jia.je>
2024-12-01 00:26:17 +0800

    Use getifaddrs to get interface names

--- entry/src/main/cpp/napi_init.cpp
+++ entry/src/main/cpp/napi_init.cpp
@@ -1,11 +1,14 @@
 #include "napi/native_api.h"
+#include <sys/types.h>
+#include <ifaddrs.h>
+#include <vector>
+#include <string>
 
-static napi_value Add(napi_env env, napi_callback_info info)
-{
+static napi_value Add(napi_env env, napi_callback_info info) {
     size_t argc = 2;
     napi_value args[2] = {nullptr};
 
-    napi_get_cb_info(env, info, &argc, args , nullptr, nullptr);
+    napi_get_cb_info(env, info, &argc, args, nullptr, nullptr);
 
     napi_valuetype valuetype0;
     napi_typeof(env, args[0], &valuetype0);
@@ -23,15 +26,39 @@ static napi_value Add(napi_env env, napi_callback_info info)
     napi_create_double(env, value0 + value1, &sum);
 
     return sum;
-
 }
 
+static napi_value GetIfAddrs(napi_env env, napi_callback_info info) {
+    struct ifaddrs *ifaddrs;
+    int res = getifaddrs(&ifaddrs);
+    std::vector<std::string> addrs;
+    if (res == 0) {
+        struct ifaddrs *p = ifaddrs;
+        while (p) {
+            addrs.push_back(p->ifa_name);
+            p = p->ifa_next;
+        }
+        freeifaddrs(ifaddrs);
+    }
+
+    napi_value arr;
+    napi_create_array_with_length(env, addrs.size(), &arr);
+
+    for (size_t i = 0; i < addrs.size(); ++i) {
+        napi_value str;
+        napi_create_string_utf8(env, addrs[i].c_str(), NAPI_AUTO_LENGTH, &str);
+        napi_set_element(env, arr, i, str);
+    }
+
+    return arr;
+}
+
+
 EXTERN_C_START
-static napi_value Init(napi_env env, napi_value exports)
-{
+static napi_value Init(napi_env env, napi_value exports) {
     napi_property_descriptor desc[] = {
-        { "add", nullptr, Add, nullptr, nullptr, nullptr, napi_default, nullptr }
-    };
+        {"add", nullptr, Add, nullptr, nullptr, nullptr, napi_default, nullptr},
+        {"getifaddrs", nullptr, GetIfAddrs, nullptr, nullptr, nullptr, napi_default, nullptr}};
     napi_define_properties(env, exports, sizeof(desc) / sizeof(desc[0]), desc);
     return exports;
 }
@@ -43,11 +70,8 @@ static napi_module demoModule = {
     .nm_filename = nullptr,
     .nm_register_func = Init,
     .nm_modname = "entry",
-    .nm_priv = ((void*)0),
-    .reserved = { 0 },
+    .nm_priv = ((void *)0),
+    .reserved = {0},
 };
 
-extern "C" __attribute__((constructor)) void RegisterEntryModule(void)
-{
-    napi_module_register(&demoModule);
-}
+extern "C" __attribute__((constructor)) void RegisterEntryModule(void) { napi_module_register(&demoModule); }
