770bfd468fa2467c9879aa71ea0bd6bcf2a99172
Jiajie Chen <c@jia.je>
2024-12-01 09:55:58 +0800

    Refactor code

--- entry/src/main/cpp/napi_init.cpp
+++ entry/src/main/cpp/napi_init.cpp
@@ -7,30 +7,6 @@
 #include <assert.h>
 #include <linux/if_link.h>
 
-static napi_value Add(napi_env env, napi_callback_info info) {
-    size_t argc = 2;
-    napi_value args[2] = {nullptr};
-
-    napi_get_cb_info(env, info, &argc, args, nullptr, nullptr);
-
-    napi_valuetype valuetype0;
-    napi_typeof(env, args[0], &valuetype0);
-
-    napi_valuetype valuetype1;
-    napi_typeof(env, args[1], &valuetype1);
-
-    double value0;
-    napi_get_value_double(env, args[0], &value0);
-
-    double value1;
-    napi_get_value_double(env, args[1], &value1);
-
-    napi_value sum;
-    napi_create_double(env, value0 + value1, &sum);
-
-    return sum;
-}
-
 // https://gist.github.com/jkomyno/45bee6e79451453c7bbdc22d033a282e
 
 char *get_ip_str(const struct sockaddr *sa, char *s, size_t maxlen) {
@@ -56,6 +32,24 @@ char *get_ip_str(const struct sockaddr *sa, char *s, size_t maxlen) {
     return s;
 }
 
+void set_object_property_string(napi_env env, napi_value obj, const char *key, const char *value) {
+    napi_value str;
+    napi_create_string_utf8(env, value, NAPI_AUTO_LENGTH, &str);
+    napi_set_named_property(env, obj, key, str);
+}
+
+void set_object_property_int32(napi_env env, napi_value obj, const char *key, int value) {
+    napi_value num;
+    napi_create_int32(env, value, &num);
+    napi_set_named_property(env, obj, key, num);
+}
+
+void set_object_property_sockaddr(napi_env env, napi_value obj, const char *key, const struct sockaddr *value) {
+    char addr_buffer[64] = {0};
+    napi_value addr_str;
+    get_ip_str(value, addr_buffer, sizeof(addr_buffer));
+    set_object_property_string(env, obj, key, addr_buffer);
+}
 
 static napi_value GetIfAddrs(napi_env env, napi_callback_info info) {
     struct ifaddrs *ifaddrs;
@@ -79,64 +73,33 @@ static napi_value GetIfAddrs(napi_env env, napi_callback_info info) {
         napi_create_object(env, &obj);
 
         // name
-        napi_value name_str;
-        napi_create_string_utf8(env, p->ifa_name, NAPI_AUTO_LENGTH, &name_str);
-        napi_set_named_property(env, obj, "name", name_str);
+        set_object_property_string(env, obj, "name", p->ifa_name);
 
         // flags
-        napi_value flags_int;
-        napi_create_int32(env, p->ifa_flags, &flags_int);
-        napi_set_named_property(env, obj, "flags", flags_int);
+        set_object_property_int32(env, obj, "flags", p->ifa_flags);
 
         if (p->ifa_addr->sa_family == AF_PACKET) {
             // handle af_packet
             if (p->ifa_data) {
                 struct rtnl_link_stats *stats = (struct rtnl_link_stats *)p->ifa_data;
 
-                napi_value tx_packets_int;
-                napi_create_int32(env, stats->tx_packets, &tx_packets_int);
-                napi_set_named_property(env, obj, "tx_packets", tx_packets_int);
-
-                napi_value tx_bytes_int;
-                napi_create_int32(env, stats->tx_bytes, &tx_bytes_int);
-                napi_set_named_property(env, obj, "tx_bytes", tx_bytes_int);
-
-                napi_value rx_packets_int;
-                napi_create_int32(env, stats->rx_packets, &rx_packets_int);
-                napi_set_named_property(env, obj, "rx_packets", rx_packets_int);
-
-                napi_value rx_bytes_int;
-                napi_create_int32(env, stats->rx_bytes, &rx_bytes_int);
-                napi_set_named_property(env, obj, "rx_bytes", rx_bytes_int);
+                set_object_property_int32(env, obj, "tx_packets", stats->tx_packets);
+                set_object_property_int32(env, obj, "tx_bytes", stats->tx_bytes);
+                set_object_property_int32(env, obj, "rx_packets", stats->rx_packets);
+                set_object_property_int32(env, obj, "rx_bytes", stats->rx_bytes);
             }
         } else {
             // addr
-            char addr_buffer[64];
-            napi_value addr_str;
-            get_ip_str(p->ifa_addr, addr_buffer, sizeof(addr_buffer));
-            napi_create_string_utf8(env, addr_buffer, NAPI_AUTO_LENGTH, &addr_str);
-            napi_set_named_property(env, obj, "addr", addr_str);
+            set_object_property_sockaddr(env, obj, "addr", p->ifa_addr);
 
             // netmask
-            char netmask_buffer[64];
-            napi_value netmask_str;
-            get_ip_str(p->ifa_netmask, netmask_buffer, sizeof(netmask_buffer));
-            napi_create_string_utf8(env, netmask_buffer, NAPI_AUTO_LENGTH, &netmask_str);
-            napi_set_named_property(env, obj, "netmask", netmask_str);
+            set_object_property_sockaddr(env, obj, "netmask", p->ifa_netmask);
 
             // broadaddr
-            char broadaddr_buffer[64];
-            napi_value broadaddr_str;
-            get_ip_str(p->ifa_broadaddr, broadaddr_buffer, sizeof(broadaddr_buffer));
-            napi_create_string_utf8(env, broadaddr_buffer, NAPI_AUTO_LENGTH, &broadaddr_str);
-            napi_set_named_property(env, obj, "broadaddr", broadaddr_str);
+            set_object_property_sockaddr(env, obj, "broadaddr", p->ifa_broadaddr);
 
             // dstaddr
-            char dstaddr_buffer[64];
-            napi_value dstaddr_str;
-            get_ip_str(p->ifa_dstaddr, dstaddr_buffer, sizeof(dstaddr_buffer));
-            napi_create_string_utf8(env, dstaddr_buffer, NAPI_AUTO_LENGTH, &dstaddr_str);
-            napi_set_named_property(env, obj, "dstaddr", dstaddr_str);
+            set_object_property_sockaddr(env, obj, "dstaddr", p->ifa_dstaddr);
         }
 
         napi_set_element(env, arr, i, obj);
@@ -151,7 +114,6 @@ static napi_value GetIfAddrs(napi_env env, napi_callback_info info) {
 EXTERN_C_START
 static napi_value Init(napi_env env, napi_value exports) {
     napi_property_descriptor desc[] = {
-        {"add", nullptr, Add, nullptr, nullptr, nullptr, napi_default, nullptr},
         {"getifaddrs", nullptr, GetIfAddrs, nullptr, nullptr, nullptr, napi_default, nullptr}};
     napi_define_properties(env, exports, sizeof(desc) / sizeof(desc[0]), desc);
     return exports;
--- entry/src/main/cpp/types/libentry/Index.d.ts
+++ entry/src/main/cpp/types/libentry/Index.d.ts
@@ -1,2 +1,1 @@
-export const add: (a: number, b: number) => number;
 export const getifaddrs: () => object[];
